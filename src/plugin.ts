f2c414457b42589a58e9313d867637fe17ff91ac
Timo Weiß <info@timo-weiss.com>
2015-05-10 15:38:29 +0200

    init socketio sample

--- src/plugin.ts
+++ src/plugin.ts
@@ -5,22 +5,47 @@ export interface IRegister {
 
 export default
 class Chat {
+    socketio:any;
+    io:any;
     constructor() {
         this.register.attributes = {
             name: 'ark-chat',
             version: '0.1.0'
         };
+
+        this.socketio = require('socket.io');
     }
 
     register:IRegister = (server, options, next) => {
         server.bind(this);
+        this.io = this.socketio(server.listener);
         this._register(server, options);
         next();
     };
 
     private _register(server, options) {
-        // Register
-        return 'register';
+        this.io.on('connection', (socket) => {
+
+            socket.emit('Oh hii!');
+
+            socket.on('burp', () => {
+                socket.emit('Excuse you!');
+            });
+        });
+
+        server.route({
+            method: 'GET',
+            path: '/getRooms',
+            config: {
+                handler: this.getRoomsRoute
+            }
+        });
+    }
+
+    getRoomsRoute(request, reply) {
+        var userId = request.auth.credentials._id;
+
+        reply({test: 'test'});
     }
 
     errorInit(error) {
